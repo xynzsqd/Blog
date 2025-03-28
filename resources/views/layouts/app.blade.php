f2d4490506da0b66fc80c720eed255e1fcd43301
xynz <sqdcontrast@gmail.com>
2025-03-28 10:04:32 +0300

    feat(ui): install bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name') }} - @yield('title')</title>
+    @vite('resources/css/app.css')
 </head>
 
 <body>
