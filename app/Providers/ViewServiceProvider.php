f7e5958ae0e99329c4e68524a75fa3bae3a551ed
xynz <sqdcontrast@gmail.com>
2025-03-29 16:09:39 +0300

    create ViewServiceProveder, change header markup

---
+++ app/Providers/ViewServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Category;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ViewServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        View::composer('partials.header', function ($view) {
+            $view->with('categories', Category::query()->withCount('posts')->get());
+        });
+    }
+}
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,38 +1,59 @@
-<header>
-    <nav>
-        <ul>
-            <li>
-                <a href="{{ route('home') }}">Home</a>
-            </li>
-            @auth
-            <li>
-                <a href="{{ route('profile.show', auth()->id()) }}">My profile</a>
-            </li>
-            @endauth
-            <li>
-                <a href="{{ route('posts.index') }}">Posts</a>
-            </li>
-            @auth
-                <li>
-                    <a href="{{ route('posts.create') }}">Create</a>
+<header class="p-3 mb-3 border-bottom">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+                <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+                    <use xlink:href="#bootstrap"></use>
+                </svg>
+            </a>
+
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="{{ route('posts.index') }}" class="nav-link px-2 link-dark">Home</a></li>
+                <li class="dropdown show">
+                    <a class="nav-link px-2 link-dark dropdown-toggle" href="#" role="button"
+                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Categories
+                    </a>
+                    <div class="dropdown-menu">
+                        @foreach ($categories as $category)
+                        <a class="dropdown-item" href="#">{{$category->name}} <span class="rounded-circle text-primary">{{$category->posts_count}}</span></a>
+                        @endforeach
+
+                    </div>
                 </li>
-            @endauth
+                @auth
+                    <li class="dropdown show">
+                        <a class="nav-link px-2 link-dark dropdown-toggle" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Profile
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">My Profile</a>
+                            <a class="dropdown-item" href="#">Edit profile</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="{{ route('posts.create') }}">Create post</a>
+                            <div class="dropdown-divider"></div>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <button type="submit" class="dropdown-item link-danger"
+                                    href="{{ route('logout') }}">Logout</button>
+                            </form>
+                        </div>
+                    </li>
+                @endauth
+            </ul>
+
+            <form method="POST" action="{{ route('posts.search') }}" class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+            </form>
+
             @guest
-                <li>
-                    <a href="{{ route('login') }}">Login</a>
-                </li>
-                <li>
-                    <a href="{{ route('register') }}">Register</a>
-                </li>
+                <ul class="nav text-end">
+                    <li><a class="nav-link px-2 link-dark" href="{{ route('login') }}">Login</a></li>
+                    <li><a class="nav-link px-2 link-dark" href="{{ route('register') }}">Register</a></li>
+                </ul>
             @endguest
-            @auth
-                <li>
-                    <form id="logoutForm" method="POST" action="{{ route('logout') }}">
-                        @csrf
-                        <a href="#" onclick="document.getElementById('logoutForm').submit();">Logout</a>
-                    </form>
-                </li>
-            @endauth
-        </ul>
-    </nav>
+
+        </div>
+    </div>
 </header>
