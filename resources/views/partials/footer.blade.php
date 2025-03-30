82c6fcc100975f2a486e2dd8b5d997ac58773d04
xynz <sqdcontrast@gmail.com>
2025-03-30 16:17:47 +0300

    feat(footer): refactor footer markup

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,5 @@
-<footer>
-    <p>Footer</p>
-</footer>
+<footer class="text-center border-top">
+    <div class="text-center py-3">
+        <p class="m-0">blog pet project</p>
+    </div>
+  </footer>
